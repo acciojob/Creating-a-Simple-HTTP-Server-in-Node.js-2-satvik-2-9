487565dbcdf86cd98232d60f99ace59b2083b662
thelastenvoy <satvikbatra@gmail.com>
2023-11-04 16:16:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
+// main.js
 const http = require('http');
+
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  // TODO: Send the ""Hello, world!"" response
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!');
 });
 
 server.listen(port, () => {
-  // TODO: Log a message to the console when the server starts listening on the port
+  console.log(`Server running on port ${port}`);
 });
-
-// DO NOT EDIT BELOW THIS LINE
-
-module.exports = { server }
